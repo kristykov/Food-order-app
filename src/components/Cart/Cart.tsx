ea804b17e77d8dcc453580fe763cf05bd7f7dd66
Krystsina Kavalevich <krikoax@gmail.com>
2022-09-06 21:43:07 +0200

    refactor: transfer assets to src

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -43,10 +43,7 @@ const Cart = ({ onClose }: ICartClose) => {
         return (
           <li className={classes["cart-item"]}>
             <div className={classes["cart-item-img"]}>
-              <img
-                src={require(`../../../assets/${item.image}`).default}
-                alt=""
-              />
+              <img src={require(`../../assets/${item.image}`).default} alt="" />
             </div>
             <div className={classes["cart-item-context"]}>
               <h3>{item.name}</h3>
