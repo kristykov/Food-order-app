833ddf2830341a32cb61847a7c5a930a2fb31a50
Krystsina Kavalevich <krikoax@gmail.com>
2022-10-04 19:41:35 +0200

    refactor home menu component

--- src/components/Layout/Home/HomeMenu.tsx
+++ src/components/Layout/Home/HomeMenu.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import HomeSpecialItem from "./HomeSpecialItem";
 import classes from "./HomeMenu.module.scss";
-import plate1 from "../../../assets/plate1.png";
-import plate2 from "../../../assets/plate2.png";
-import plate3 from "../../../assets/plate3.png";
-import plate4 from "../../../assets/plate4.png";
+
+const specialData = [
+  {
+    id: "p1",
+    name: "Barbecue salad",
+    detail: "Delicious dish",
+    price: 22,
+    img: "plate1",
+  },
+  {
+    id: "p2",
+    name: "Pasta with seafood",
+    detail: "Delicious dish",
+    price: 28,
+    img: "plate2",
+  },
+  {
+    id: "p3",
+    name: "Chicken salad",
+    detail: "Delicious dish",
+    price: 22,
+    img: "plate3",
+  },
+  {
+    id: "p4",
+    name: "Spinach salad",
+    detail: "Delicious dish",
+    price: 20,
+    img: "plate4",
+  },
+  {
+    id: "p5",
+    name: "Buffalo pizza",
+    detail: "Delicious dish",
+    price: 22,
+    img: "plate5",
+  },
+];
 
 const HomeMenu = () => {
   return (
@@ -13,53 +48,18 @@ const HomeMenu = () => {
         <h2 className="section-title">Our best menu</h2>
       </div>
       <div className={`${classes["menu-container"]} container`}>
-        <div className={classes["menu-content"]}>
-          <img src={plate1} className={classes["menu-img"]} alt="" />
-          <div className={classes["menu-card"]}>
-            <h3 className={classes["menu-name"]}>Barbecue salad</h3>
-            <span className={classes["menu-detail"]}>Delicious dish</span>
-            <span className={classes["menu-price"]}>$22.00</span>
-            <a className={classes["menu-btn"]} href="/pic">
-              Find out more
-            </a>
-          </div>
-        </div>
-
-        <div className={classes["menu-content"]}>
-          <img src={plate2} className={classes["menu-img"]} alt="" />
-          <div className={classes["menu-card"]}>
-            <h3 className={classes["menu-name"]}>Pasta with seafood</h3>
-            <span className={classes["menu-detail"]}>Delicious dish</span>
-            <span className={classes["menu-price"]}>$22.00</span>
-            <a className={classes["menu-btn"]} href="/pic">
-              Find out more
-            </a>
-          </div>
-        </div>
-
-        <div className={classes["menu-content"]}>
-          <img src={plate3} className={classes["menu-img"]} alt="" />
-          <div className={classes["menu-card"]}>
-            <h3 className={classes["menu-name"]}>Chicken salad</h3>
-            <span className={classes["menu-detail"]}>Delicious dish</span>
-            <span className={classes["menu-price"]}>$22.00</span>
-            <a className={classes["menu-btn"]} href="/pic">
-              Find out more
-            </a>
-          </div>
-        </div>
-
-        <div className={classes["menu-content"]}>
-          <img src={plate4} className={classes["menu-img"]} alt="" />
-          <div className={classes["menu-card"]}>
-            <h3 className={classes["menu-name"]}>Spinach salad</h3>
-            <span className={classes["menu-detail"]}>Delicious dish</span>
-            <span className={classes["menu-price"]}>$22.00</span>
-            <a className={classes["menu-btn"]} href="/pic">
-              Find out more
-            </a>
-          </div>
-        </div>
+        {specialData.map((item) => {
+          return (
+            <HomeSpecialItem
+              id={item.id}
+              key={item.id}
+              name={item.name}
+              detail={item.detail}
+              price={item.price}
+              img={item.img}
+            />
+          );
+        })}
       </div>
     </section>
   );
---
+++ src/components/Layout/Home/HomeSpecialItem.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import classes from "./HomeSpecialItem.module.scss";
+
+interface ISpecialItem {
+  id: string;
+  name: string;
+  detail: string;
+  price: number;
+  img: string;
+}
+
+const HomeSpecialItem = ({ id, name, detail, price, img }: ISpecialItem) => {
+  return (
+    <div className={classes["menu-content"]} key={id}>
+      <img
+        src={require(`../../../assets/${img}.png`).default}
+        className={classes["menu-img"]}
+        alt=""
+      />
+      <div className={classes["menu-card"]}>
+        <h3 className={classes["menu-name"]}>{name}</h3>
+        <span className={classes["menu-detail"]}>{detail}</span>
+        <span className={classes["menu-price"]}>{`$${price}`}</span>
+        <a className={classes["menu-btn"]} href="/pic">
+          Find out more
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSpecialItem;
